c0c3649ea507a7b9d1c41f67be20415527bc938b
Raoul Shah <74040700+Raoul-Shah@users.noreply.github.com>
2021-03-27 00:40:17 +0100

    added JavaScript (final step)

--- script.js
+++ script.js
@@ -4,6 +4,30 @@ function getMarks() {
     var english = Number(document.getElementById("english").value);
     var chemistry = Number(document.getElementById("chemistry").value);
     var physics = Number(document.getElementById("physics").value);
+    var totalMarks = 400;
+    var MarksObtained = maths + english + chemistry + physics;
+    var percentage = (MarksObtained / totalMarks) * 100;
+    var grade = "";
+    document.getElementById("percentage").innerHTML = percentage + "%"
+   
+   if(percentage > 90){
+       grade = "A+";
+   } else if(percentage < 90 && percentage > 80) {
+       grade = "A";
+   } else if(percentage < 80 && percentage > 60){
+       grade = "B";
+   } else if (percentage < 60) {
+       grade = "F"
+   }
+   
+   document.getElementById("grade").innerHTML = grade;
+   if(percentage < 60){
+     document.getElementById("pass").innerHTML = "Sorry! you failed! please try next time";
+   } else {
+     document.getElementById("pass").innerHTML = "Congratulations! you have passed";
+   }
 
-    console.log(maths + english + chemistry + physics);
+   
+    console.log(percentage + "%");
+    console.log(grade);
 }
