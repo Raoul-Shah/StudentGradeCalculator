f8b9a967ff660df79af27f5fc5b901a43e7f17f6
Raoul Shah <74040700+Raoul-Shah@users.noreply.github.com>
2021-03-26 23:22:27 +0100

    added HTML and CSS

---
+++ script.js
@@ -0,0 +1,9 @@
+function getMarks() {
+
+    var maths = Number(document.getElementById("maths").value);
+    var english = Number(document.getElementById("english").value);
+    var chemistry = Number(document.getElementById("chemistry").value);
+    var physics = Number(document.getElementById("physics").value);
+
+    console.log(maths + english + chemistry + physics);
+}
